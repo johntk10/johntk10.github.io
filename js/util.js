bf7e0a6efb8d46bc14def4d6f6ac45d2939e9c83
johntk10 <jtkutbay@gmail.com>
2023-11-23 21:43:22 -0500

    main design

--- js/util.js
+++ js/util.js
@@ -1,26 +1,9 @@
 function scrollToPosition(position) {
     window.scrollTo({
         top: position,
-        behavior: 'smooth'
+        behavior: 'smooth',
+        duration: 2000
     });
 }
 
-let lastScrollTop = 0;
-const header = document.getElementById('header');
-
-function handleScroll() {
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (scrollTop > lastScrollTop) {
-    header.style.opacity = '0.5';
-  } else {
-    header.style.opacity = '0.8';
-  }
-
-  lastScrollTop = scrollTop;
-}
-
-window.addEventListener('scroll', () => {
-  handleScroll();
-});
 
